b845679b7a35d903661b0b13114d2537948ce7e6
Dmitriy Shadrin (LNX) <dshadrin@gmail.com>
2018-06-02 11:12:34 +1000

    fix linux build

--- build_linux_makefiles.sh
+++ build_linux_makefiles.sh
@@ -3,4 +3,5 @@ rm -rf build
 rm -rf bin
 mkdir build
 cd build
-cmake .. -G"Unix Makefiles" -DPRJ_BUILD_TARGET:STRING="linux" -DPRJ_BUILD_TYPE:STRING="Release" -DPRJ_INSTALL_DIR:STRING="install"
+cmake .. -G "Unix Makefiles" -DPRJ_INSTALL_DIR:STRING="install"
+
